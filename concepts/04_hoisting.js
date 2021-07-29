4a89e3c6bae02c92aaa9dbef829930a56cf75a72
Abdulaziz <abdulazizfaactory@gmail.com>
2021-07-29 23:10:28 +0500

    hoisting of vars and functions

---
+++ concepts/04_hoisting.js
@@ -0,0 +1,22 @@
+// console.log(sum(1, 41)); 
+
+// function sum(a, b) {
+//     return a + b;
+// }
+
+// console.log(i); // undefined 
+
+// var i = 41;
+
+// console.log(i);
+// if var variable is not defined then it is undefined 
+
+// difference between var and let
+
+// console.log(num); // ReferenceError: Cannot access 'num' before initialization
+// const num = 42; 
+// console.log(num);
+// // let and const can't be hoisted, so It gives an error
+
+// 
+
